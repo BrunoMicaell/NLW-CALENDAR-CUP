fcb608b0a95675aa8e7fae499acdcccb087d94ef
Vitor Assumpcao <vitorassumpcao0@gmail.com>
2022-11-28 10:55:14 -0300

    Teste

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ function createGame(player1, hour, player2) {
   `
 }
 
-let delay = -0.4;
+let delay = -0.4
 function createCard(date, day, games) {
-  delay = delay + 0.4;
+  delay = delay + 0.4
   return `
     <div class="card" style="animation-delay:${delay}s">
         <h2>${date} <span>${day}</span></h2>
@@ -25,4 +25,8 @@ function createCard(date, day, games) {
 document.querySelector("#cards").innerHTML =
   createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
   createCard("28/11", "segunda", createGame("switzerland", "13:00", "brazil")) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon")) +
+  createCard("02/12", "sexta", createGame("switzerland", "16:00", "cameroon")) 
+
+
+
